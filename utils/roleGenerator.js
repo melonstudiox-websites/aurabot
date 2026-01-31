aeee84c47da63693bbbcd50776bd296d9263e549
melonstudiox-websites <melonstudiox@gmail.com>
2026-01-31 10:18:28 +0800

    Update roleGenerator.js

--- utils/roleGenerator.js
+++ utils/roleGenerator.js
@@ -1,1 +1,14 @@
+import { FONTS, WRAPPERS, ROLE_COLORS } from '../config.js';
+
+export function generateRoleName(tier, options = {}) {
+  const font = options.font || FONTS[Math.floor(Math.random() * FONTS.length)];
+  const wrapper = options.wrapper || WRAPPERS[Math.floor(Math.random() * WRAPPERS.length)];
+  const emoji = options.emoji || ""; 
+
+  return `${wrapper[0]} ${emoji} ${font} ${tier.toUpperCase()} ${emoji} ${wrapper[1]}`.trim();
+}
+
+export function generateRoleColor(tier) {
+  return ROLE_COLORS[tier] || 0x99AAB5;
+}
 
