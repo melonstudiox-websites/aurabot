7e6d79618e091136585b85faf9ff7fbc64673c0b
melonstudiox-websites <melonstudiox@gmail.com>
2026-01-31 10:18:40 +0800

    Update channelGenerator.js

--- utils/channelGenerator.js
+++ utils/channelGenerator.js
@@ -1,1 +1,8 @@
+export function generateCategory(name) {
+  return { name, type: 4 }; // 4 = Category
+}
+
+export function generateChannel(name, type="text", category=null, permissions={}) {
+  return { name, type, category, permissions };
+}
 
